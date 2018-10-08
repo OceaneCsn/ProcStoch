c7fb5611544ae4f4205d31599198c18f6f803e46
Oceane Csn <oceane.cassan@insa-lyon.fr>
2018-10-08 15:57:11 +0200

    tp Poisson corrected

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import math
+import random as rd
 #sys.path.append('/home/ocassan/ProcStoch')
 
 ############################# Simple functions ##########################
@@ -30,7 +31,8 @@ def lam_estimate(poisson):
 
 ############################# Neuron pikes ##########################
 
-pathToFiles = "D:/ProcStoch"
+#pathToFiles = "D:/ProcStoch"
+pathToFiles = "/home/ocassan/ProcStoch/"
 sham4_art = pd.read_csv(os.path.join(pathToFiles, 'SHAM4_artefact_timing.txt'), header = None, sep = '\t')
 sham5_art = pd.read_csv(os.path.join(pathToFiles, 'SHAM5_artefact_timing.txt'), header = None, sep = '\t')
 sham4_spikes = pd.read_csv(os.path.join(pathToFiles, 'SHAM4_spike_timing.txt'), header = None, sep = '\t')
@@ -77,12 +79,12 @@ for values in data:
 print(1/np.mean(concat))
 
 #checking the fit on the plot
-histogram_results = plt.hist(concat, bins = 70)
+histogram_results = plt.hist(concat, bins = 100)
 x = np.linspace(0,0.10, num = 100)
 plt.plot(x, [1.0/0.0337*math.exp(-xs/0.0337) for xs in x], 'r-')
 
 #wrong!!! lambda depends on t and must be estimated for every interval... 
-#on fait la moyenne des counts divisée par le temps sur chaque bin de l'histogramme
+#on fait la moyenne des counts divisée par le temps sur chaque bin de l'histogramme pour avoir les lambda de t
 #Sergio's code (on peut aussi le faire avec une régression de random forest) :
 counts = pd.DataFrame()
 nb_experiments = 50
@@ -90,11 +92,18 @@ mean_count = histogram_results[0]/nb_experiments
 t = histogram_results[1][1:] - histogram_results[1][:-1]
 counts["lambda"] = mean_count/(t)
 window_size = 5
+#smoothing
 z = counts.rolling(window_size, center=True, win_type="triang",min_periods=int(window_size*1/4))
 Lambda_t = z.mean()
 plt.plot(Lambda_t ,"o")
 plt.xlabel("$Time$ $t$ $(\cdot 10^{-3} s)$",fontsize=20)
 plt.ylabel("$\lambda(t)$",fontsize=20)
+
+
+
+#regression model 
+from sklearn.ensemble import RadomForestRegressor
+rf=RandomForestRegressor(n_estimators=50, criterion='mse', max_depth=5)
 
 ################ Simultaing a non homogenous Poisson Process
 #my idea : for a given number of T experiments for 1000 replicates, pick a value in a poisson law
@@ -116,7 +125,24 @@ poiss = [d[d<0.1] for d in poiss]
 poiss = [d[d>0] for d in poiss]
 for i,d in enumerate(poiss): 
     plt.plot(d, np.repeat(i,len(d)), 'o')
-    
 
+
+##premiere methode
+values = [0]    
+for t in range(0:100):
+    values.append(0)
     
+#deuxieme methode : separation. Pour chaque evenement, on le mets dans un des 2 processus de poisson (on garde le spike ou pas)
+#suivant une loi de Bernouilli. On tire dans une loi uniforme entre 0 et 0.1. On le garde suivant une proba qui dépend de la valeur piochée
+
+spikes =  []
+for i in range(0,10000):
+    spike = rd.random()*100
+    prob = Lambda_t['lambda'][int(spike)]/max(Lambda_t['lambda'])
+    if rd.random()<prob:
+        spikes.append(spike)
+
+histogram_results = plt.hist(spikes, bins = 100)
+
+Lambda_t['lambda']
     
